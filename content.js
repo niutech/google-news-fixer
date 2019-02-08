c346bb0c20a9569290d417dc1d26bdfc90814ee6
niu tech <jerzyglowacki@gmail.com>
2019-02-08 01:09:28 +0100

    Version 0.4
    Fixed stories selector

--- content.js
+++ content.js
@@ -4,14 +4,14 @@
 
     var maxStories = 5;
     var includedTopics = {};
-    var storiesSelector = 'main > c-wiz > div:first-child > div:nth-child(-n+5)';
+    var storiesSelector = 'main > c-wiz[data-n-q] > div:first-child > div:nth-child(-n+5), main > c-wiz main > div:first-child > div:nth-child(-n+5)';
     var targetSectionSelector = 'main > c-wiz > div';
     var topicsSelector = '#library a[href^="./topics"], #library a[href^="./publications"], #searches a[href^="./search"]';
     var titleSelector = 'h2';
     var targetTitleSelector = 'main h2';
     var searchTitleSelector = 'main > c-wiz';
-    var topicsUrl = '/my/library' + location.search;
-    var searchesUrl = '/my/searches' + location.search;
+    var topicsUrl = location.origin + '/my/library' + location.search;
+    var searchesUrl = location.origin + '/my/searches' + location.search;
     var options = {credentials: 'same-origin', cache: 'default'};
     var alreadyRemovedTopics = false;
 
@@ -24,8 +24,7 @@
     }
 
     function process(text) {
-        var html = new DOMParser().parseFromString(text, 'text/html');
-        html.documentElement.querySelectorAll(topicsSelector).forEach(function (topic) {
+        new DOMParser().parseFromString(text, 'text/html').documentElement.querySelectorAll(topicsSelector).forEach(function (topic) {
             fetch(topic.href, options).then(convert).then(function (text) {
                 var html = new DOMParser().parseFromString(text, 'text/html');
                 // Get source title
@@ -44,24 +43,29 @@
                 var targetSection = document.querySelector(targetSectionSelector);
                 targetSection.appendChild(targetHeading);
                 // Insert stories
-                var stories = html.documentElement.querySelectorAll(storiesSelector.replace('5', maxStories));
+                var stories = html.documentElement.querySelectorAll(storiesSelector.replace(/5/g, maxStories));
                 stories.forEach(function (story) {
                     story.classList.add('gnf-added');
                     targetSection.appendChild(story);
                 });
-                // Update topics & remove excluded ones
-                if (!alreadyRemovedTopics) {
-                    var newIncludedTopics = {};
-                    document.querySelectorAll('.' + document.querySelector(targetTitleSelector).parentNode.parentNode.className.replace(/\s+/g, '.') + ':not(.gnf-added)').forEach(function (el, i) {
-                        var key = el.querySelector(targetTitleSelector).innerText.trim();
-                        newIncludedTopics[key] = typeof includedTopics[key] === 'boolean' ? includedTopics[key] : true;
-                        el.classList.add('gnf-head-' + i, newIncludedTopics[key] ? 'gnf-visible' : 'gnf-hidden');
-                    });
-                    chrome.storage.sync.set({maxStories: maxStories, includedTopics: newIncludedTopics});
-                    alreadyRemovedTopics = true;
-                }
+                removeTopics();
             });
         });
+    }
+
+    function removeTopics() {
+        if (alreadyRemovedTopics) {
+            return;
+        }
+        var newIncludedTopics = {};
+        document.querySelectorAll('.' + document.querySelector(targetTitleSelector).parentNode.parentNode.className.replace(/\s+/g, '.') + ':not(.gnf-added)').forEach(function (el, i) {
+            var key = el.querySelector(targetTitleSelector).innerText.trim();
+            newIncludedTopics[key] = typeof includedTopics[key] === 'boolean' ? includedTopics[key] : true;
+            el.classList.add('gnf-head-' + i, newIncludedTopics[key] ? 'gnf-visible' : 'gnf-hidden');
+        });
+        console.log('Topics:', newIncludedTopics);
+        chrome.storage.sync.set({maxStories: maxStories, includedTopics: newIncludedTopics});
+        alreadyRemovedTopics = true;
     }
 
     function init(config) {
@@ -69,6 +73,7 @@
         includedTopics = Object.prototype.toString.call(config.includedTopics) === '[object Object]' && config.includedTopics || includedTopics;
         fetch(topicsUrl, options).then(convert).then(process);
         fetch(searchesUrl, options).then(convert).then(process);
+        addEventListener('load', removeTopics);
     }
 
     chrome.storage.sync.get(['maxStories', 'includedTopics'], init);
